12abb7355cec249cff6aa449fd0c11e910f08a73
wangshiduo <710177617@qq.com>
2017-03-12 22:27:45 +0800

    remote_master-1

--- app/src/main/java/com/example/wshiduo/mycs/FullscreenActivity.java
+++ app/src/main/java/com/example/wshiduo/mycs/FullscreenActivity.java
@@ -76,7 +76,7 @@ public class FullscreenActivity extends AppCompatActivity {
     }
 
     private void CS() {
-        Log.d("1", "LocalMaster");
+        Log.d("1", "Remote_Master");
     }
 
     @Override
