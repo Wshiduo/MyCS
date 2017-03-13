89fd3b6fbbb81830311563ad0c1fa869021752e2
wangshiduo03 <710177617@qq.com>
2017-03-13 08:58:32 +0800

    多次commit到同一个branch上————1

--- app/src/main/java/com/example/wshiduo/mycs/FullscreenActivity.java
+++ app/src/main/java/com/example/wshiduo/mycs/FullscreenActivity.java
@@ -81,6 +81,7 @@ public class FullscreenActivity extends AppCompatActivity {
         Log.d("2", "Remote_Master to 2 new remote_Branch");
         Log.d("3", "Remote_Master to 2 new remote_Branch");
         Log.d("4", "Remote_Master to 2 new remote_Branch");
+        Log.d("5", "Remote_Master to 2 new remote_Branch");
     }
 
     @Override
