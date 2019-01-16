e4e259a24cb7bbc37ac7a4d4054b1c1b147b7940
王世铎 <710177617@qq.com>
2019-01-16 18:00:08 +0800

    amend -m

--- app/src/main/java/com/example/wshiduo/mycs/Cs.java
+++ app/src/main/java/com/example/wshiduo/mycs/Cs.java
@@ -1,8 +1,20 @@
 package com.example.wshiduo.mycs;
 
+import android.widget.Toast;
+import java.util.logging.Logger;
+
 /**
  * 创建人：王世铎
  * 创建日期：2019/1/16
  * 描  述：
  * 版本号：1.0
  */
+public class Cs {
+
+  public void cSIt(){
+    Toast.makeText(null, "", Toast.LENGTH_SHORT);
+    Toast.makeText(null, "", Toast.LENGTH_SHORT);
+
+
+  }
+}
