15b296a7e1d8eced335a54bf42c6b0fd5ac54d5a
王世铎 <710177617@qq.com>
2019-02-15 15:31:32 +0800

    测试m

---
+++ app/src/main/java/com/example/wshiduo/mycs/NoCashTwo.java
@@ -0,0 +1,18 @@
+package com.example.wshiduo.mycs;
+
+import android.util.Log;
+import java.util.logging.Logger;
+
+/**
+ * 创建人：王世铎
+ * 创建日期：2019/1/16
+ * 描  述：
+ * 版本号：1.0
+ */
+public class NoCash {
+
+  public void doIt(){
+    Logger.getLogger("");
+    Logger.getLogger("");
+  }
+}
